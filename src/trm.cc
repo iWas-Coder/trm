d5db8240c9b32af9c08e0ddf4735a5c3c6b23849
iWas-Coder <wasymatieh01@gmail.com>
2024-06-26 15:30:43 +0200

    fix: compacted test insertion of hashed strings

--- src/trm.cc
+++ src/trm.cc
@@ -7,15 +7,11 @@ int main(int argc, char **argv) {
   args.printFlags();
   args.printFiles();
 
-  auto hash1 { trm::merkle::Hash<trm::merkle::DigestType::SHA256>::compute("Hello, World!") };
-  auto hash2 { trm::merkle::Hash<trm::merkle::DigestType::SHA256>::compute("Welcome to San Andreas...") };
-  auto hash3 { trm::merkle::Hash<trm::merkle::DigestType::SHA256>::compute("This is C++ and its cool :D") };
-  auto hash4 { trm::merkle::Hash<trm::merkle::DigestType::SHA256>::compute("Leaks are scheiße") };
   trm::merkle::Tree<trm::merkle::DigestType::SHA256> tree;
-  tree.insert(hash1);
-  tree.insert(hash2);
-  tree.insert(hash3);
-  tree.insert(hash4);
+
+  for (const auto &i : { "Hello", "World", "This is", "C++" }) {
+    tree.insert(trm::merkle::Hash<trm::merkle::DigestType::SHA256>::compute(i));
+  }
 
   std::cout << "Merkle root: " << tree.root() << std::endl;
 
