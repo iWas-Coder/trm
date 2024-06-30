831b6221082516f50591d8f46df6116df38bfe37
iWas-Coder <wasymatieh01@gmail.com>
2024-06-30 18:53:44 +0200

    Added `GPL-3.0` license and notice to all relevant files

--- src/args.cc
+++ src/args.cc
@@ -1,3 +1,24 @@
+/*
+ * GNU trm --- A transactional POSIX’s rm command
+ * Copyright (C) 2024 Wasym A. Alonso
+ *
+ * This file is part of trm.
+ *
+ * trm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * trm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with trm.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 #include <args.hh>
 #include <unistd.h>
 #include <iostream>
