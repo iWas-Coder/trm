ed9e022fda7c145e0775f0a1648055eabb51fb92
iWas-Coder <wasymatieh01@gmail.com>
2024-06-30 18:27:11 +0200

    fix: compacted unnecessary whitespaces

--- src/trm.cc
+++ src/trm.cc
@@ -8,8 +8,8 @@ int main(int argc, char **argv) {
   args.printFiles();
 
   std::vector<trm::chain::TX<trm::merkle::DigestType::SHA256>> txns;
-  for (const auto &i : args.getFiles()) txns.emplace_back(trm::chain::TX<trm::merkle::DigestType::SHA256> { i });
-  trm::chain::Block<trm::merkle::DigestType::SHA256> block { txns };
+  for (const auto &i : args.getFiles()) txns.emplace_back(trm::chain::TX<trm::merkle::DigestType::SHA256> {i});
+  trm::chain::Block<trm::merkle::DigestType::SHA256> block {txns};
 
   std::cout << std::endl;
   block.print();
